9be6382ff0826c565447397ded361b4af48c2891
Hector A. Romero <hectorromero.developer@gmail.com>
2020-06-23 09:34:25 -0700

    [ADD] Subscriptions and catching db ready state

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { SQLiteObject, SQLite } from '@ionic-native/sqlite/ngx';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Platform } from '@ionic/angular';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  private database: SQLiteObject;
+  private dbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor() { }
+  developers = new BehaviorSubject([]);
+  products = new BehaviorSubject([]);
+
+  constructor(
+    private platform: Platform,
+    private sqlitePorter: SQLitePorter,
+    private sqlite: SQLite,
+    private http: HttpClient
+  ) {
+    // Check if our platform is ready to use it
+    this.platform.ready()
+      .then(() => {
+        // Create database locally
+        this.sqlite.create({
+          name: 'developers.db',
+          location: 'default'
+        })
+          .then((db: SQLiteObject) => {
+            this.database = db;
+            this.seedDatabase(); // Add dummy data
+          });
+      });
+  }
+
+  seedDatabase() {
+    this.http.get('assets/seed.sql', { responseType: 'text' })
+      .subscribe(sql => {
+        this.sqlitePorter.importSqlToDb(this.database, sql)
+          .then(_ => {
+            this.loadDevelopers();
+            this.loadProducts();
+            this.dbReady.next(true);
+          })
+          .catch(e => console.log(e));
+      });
+  }
+
+  getDatabaseState() {
+    return this.dbReady.asObservable();
+  }
+
+  getDevs(): Observable<Dev[]> {
+    return this.developers.asObservable();
+  }
+
+  getProducts(): Observable<any[]> {
+    return this.products.asObservable();
+  }
+}
+
+export interface Dev {
+  id: number;
+  name: string;
+  skills: any[];
+  img: string;
 }
