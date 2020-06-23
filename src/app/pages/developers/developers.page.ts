b62b7acdbc727c4d5d71fa4f1abe0120c29d89d5
Hector A. Romero <hectorromero.developer@gmail.com>
2020-06-23 13:00:24 -0700

    [ADD] Interface user

--- src/app/pages/developers/developers.page.ts
+++ src/app/pages/developers/developers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dev, DatabaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-developers',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./developers.page.scss'],
 })
 export class DevelopersPage implements OnInit {
+  developers: Dev[] = [];
 
-  constructor() { }
+  developer = {};
+  product = {};
+
+  selectedView = 'devs';
+
+  constructor(private db: DatabaseService) { }
 
   ngOnInit() {
+    this.db.getDatabaseState().subscribe(ready => {
+      if (ready) {
+        this.db.getDevs().subscribe(devs => {
+          this.developers = devs;
+        });
+        this.product = this.db.getProducts();
+      }
+    });
+  }
+
+  addDeveloper() {
+    let skills = this.developer['skills'].split(',');
+    skills = skills.map(skill => skill.trim());
+    this.db.addDeveloper(this.developers['name'], skills, this.developers['img'])
+      .then(_ => {
+        this.developer = {};
+      });
+  }
+
+  addProduct() {
+    this.db.addProduct(this.product['name'], this.product['creator'])
+      .then(_ => {
+        this.product = {};
+      });
   }
 
 }
